97668f21def152377dd68fabc6d17db47a3c140c
lancebeet <lancebeet@users.noreply.github.com>
2018-03-27 10:46:45 +0200

    testing more features in example

--- example.jl
+++ example.jl
@@ -8,15 +8,18 @@ end
 
 canvas = Canvas()
 bg = dom"svg:rect[height=800, width=800, fill=blue]"()
-box1 = dom"svg:rect[id=box1, height=50, width=50, x=50, y=50]"()
-box2 = dom"svg:rect[id=box2, height=100, width=25, x=250, y=250]"()
-box3 = dom"svg:rect[id=box3, height=100, width=25, x=550, y=350]"()
-circ1 = dom"svg:circle[id=circ1, cx=200, cy=25, r=100]"()
+box1 = dom"svg:rect[id=box1, height=50, width=50, x=50, y=50, fill=yellow]"()
+box2 = dom"svg:rect[id=box2, height=100, width=25, x=250, y=250, fill=green]"()
+box3 = dom"svg:rect[id=box3, height=100, width=25, x=550, y=350, fill=purple]"()
+box4 = dom"svg:rect[id=box4, height=40, width=50, x=700, y=200, fill=red]"()
+circ1 = dom"svg:circle[id=circ1, cx=200, cy=25, r=100, fill=orange]"()
+
 addstatic!(canvas, bg)
-addmovable!(canvas, box1)
+addmovable!(canvas, box1, "x")
 addmovable!(canvas, box2)
 addmovable!(canvas, circ1)
 addclickable!(canvas, box3)
+addstatic!(canvas, box4)
 
 on(canvas.selection) do val
     println("selected $val")
